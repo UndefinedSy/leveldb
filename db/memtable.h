11e301e0195bfde9cfd1f161fe35d0742582a0d0
Yao Shunyu <shunyu.sean@gmail.com>
2021-12-08 00:52:17 +0800

    Update memtable.h

--- db/memtable.h
+++ db/memtable.h
@@ -17,69 +17,68 @@ namespace leveldb {
 class InternalKeyComparator;
 class MemTableIterator;
 
+// MemTables are reference counted.
+// The initial reference count is 0
+// the caller must call Ref() at least once.
 class MemTable {
- public:
-  // MemTables are reference counted.  The initial reference count
-  // is zero and the caller must call Ref() at least once.
-  explicit MemTable(const InternalKeyComparator& comparator);
+public:
+	explicit MemTable(const InternalKeyComparator& comparator);
 
-  MemTable(const MemTable&) = delete;
-  MemTable& operator=(const MemTable&) = delete;
+	MemTable(const MemTable&) = delete;
+	MemTable& operator=(const MemTable&) = delete;
 
-  // Increase reference count.
-  void Ref() { ++refs_; }
+	// Increase reference count.
+	void Ref() { ++refs_; }
 
-  // Drop reference count.  Delete if no more references exist.
-  void Unref() {
-    --refs_;
-    assert(refs_ >= 0);
-    if (refs_ <= 0) {
-      delete this;
-    }
-  }
+	// Drop reference count.  Delete if no more references exist.
+	void Unref() {
+		--refs_;
+		assert(refs_ >= 0);
+		if (refs_ <= 0) {
+			delete this;
+		}
+  	}
 
-  // Returns an estimate of the number of bytes of data in use by this
-  // data structure. It is safe to call when MemTable is being modified.
-  size_t ApproximateMemoryUsage();
+	// Returns an estimate of the number of bytes of data in use by this
+	// data structure.
+	// It is safe to call when MemTable is being modified.
+	size_t ApproximateMemoryUsage();
 
-  // Return an iterator that yields the contents of the memtable.
-  //
-  // The caller must ensure that the underlying MemTable remains live
-  // while the returned iterator is live.  The keys returned by this
-  // iterator are internal keys encoded by AppendInternalKey in the
-  // db/format.{h,cc} module.
-  Iterator* NewIterator();
+	// 调用者必须确保在这个返回的 Iterator 仍存活时，底层 MemTable 也应保持存活。
+	// 此 Iterator 返回的 key 是由 db/format.{h,cc} 模块中的 AppendInternalKey 编码的 internal keys。
+	// 
+	// @return, 一个获取 memtable 内容的 iterator。
+	Iterator* NewIterator();
 
-  // Add an entry into memtable that maps key to value at the
-  // specified sequence number and with the specified type.
-  // Typically value will be empty if type==kTypeDeletion.
-  void Add(SequenceNumber seq, ValueType type, const Slice& key,
-           const Slice& value);
+	// Add an entry into memtable that maps key to value at the
+	// specified sequence number and with the specified type.
+	// Typically value will be empty if type==kTypeDeletion.
+	void Add(SequenceNumber seq, ValueType type,
+			 const Slice& key, const Slice& value);
 
-  // If memtable contains a value for key, store it in *value and return true.
-  // If memtable contains a deletion for key, store a NotFound() error
-  // in *status and return true.
-  // Else, return false.
-  bool Get(const LookupKey& key, std::string* value, Status* s);
+	// If memtable contains a value for key, store it in *value and return true.
+	// If memtable contains a deletion for key, store a NotFound() error in *status and return true.
+	// Else, return false.
+	bool Get(const LookupKey& key, std::string* value, Status* s);
 
- private:
-  friend class MemTableIterator;
-  friend class MemTableBackwardIterator;
+private:
+	friend class MemTableIterator;
+	friend class MemTableBackwardIterator;
 
-  struct KeyComparator {
-    const InternalKeyComparator comparator;
-    explicit KeyComparator(const InternalKeyComparator& c) : comparator(c) {}
-    int operator()(const char* a, const char* b) const;
-  };
+	struct KeyComparator {
+		const InternalKeyComparator comparator;
+		explicit KeyComparator(const InternalKeyComparator& c) : comparator(c) {}
+		int operator()(const char* a, const char* b) const;
+	};
 
-  typedef SkipList<const char*, KeyComparator> Table;
+	typedef SkipList<const char*, KeyComparator> Table;
 
-  ~MemTable();  // Private since only Unref() should be used to delete it
+	~MemTable();  // Private since only Unref() should be used to delete it
 
-  KeyComparator comparator_;
-  int refs_;
-  Arena arena_;
-  Table table_;
+	KeyComparator comparator_;
+	int refs_;
+	Arena arena_;
+	Table table_;	// skipList
 };
 
 }  // namespace leveldb
